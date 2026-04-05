0aa7174a3809989eacb49fe2c72ba25d6e53567d
erickNA2 <erick.araujo@sptech.school>
2026-04-05 13:15:55 -0300

    module hotfix

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Typography from "@/components/ui/Typography";
-import Card from "@/components/ui/Card";
+import { Card } from "@/components/ui";
 import Link from "next/link";
 
 export default function AboutSection() {
---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { Button } from './Button';
+export { default as Typography } from './Typography';
+export { default as Window } from './Window';
