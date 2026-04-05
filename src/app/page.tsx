9ffe295c7db3e206dc538b58a77e411ac51449f8
erickNA2 <erick.araujo@sptech.school>
2026-04-05 13:07:22 -0300

    hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import React from "react";
 import HeroSection from "@/components/sections/HeroSection";
-import ServicesSection from "@/components/sections/ServicesSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
+import ServicesSection from "@/components/sections/ServicesSection";
 import AboutSection from "@/components/sections/AboutSection";
 import ProcessSection from "@/components/sections/ProcessSection";
 import { useLocomotiveScroll } from "@/hooks/useLocomotiveScroll";
